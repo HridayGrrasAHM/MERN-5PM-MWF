455b7dd9843ffa2c5af3c722375bcbd49819ec76
hridaygoswami <epikhriday@gmail.com>
2023-11-17 15:05:46 +0530

    adding files for day0 to day14

---
+++ day13/JS Basics.js
@@ -0,0 +1,84 @@
+// console.log("Hello world")
+// console.log("Hello world.")
+// console.log("How are you?")
+
+// special keywords: \ -> \n: new line, \t: tab space, \\: \ print
+// console.log("Hello world.\nHow are you?")
+// console.log("Hello world.\tHow are you?")
+// console.log("Hello world.\\ How are you?")
+
+// combining 2 strings
+// console.log("Hello world"+"How are you?")  // concat method
+// console.log("Hello world", "How are you?") // normal method
+
+
+// maths ops
+// console.log(2+3)
+// console.log(2 - 3)
+// console.log(2 / 3)
+// console.log(2 * 3)
+
+
+// combining strings with maths ops
+// console.log("If we add", 2, "with", 3, "we get", 2+3)
+
+// formating method
+// console.log(`If we add ${2} with ${3} we get ${2+3}`)
+
+
+// difference in datatype
+// variable (7th maths) = datatype
+// 2 types of datatypes: primitive(created with the original lang) & non-primitive(updated/added later on)
+
+// primitive datatype:
+/* 
+    1. int (number)
+    2. float (decimal)
+    3. string (text)
+    4. boolean (true or false)
+*/
+
+// x = 10
+// console.log(x)
+// console.log("The value of x is ->", x)
+// console.log("The value of x is => "+x)
+// var-name (x) = datatype (int) : value (10)
+
+
+
+
+// how to use plus
+// console.log(3+3)
+// console.log(3+"3")
+// console.log("3"+ +3)
+// console.log(3 + + "3")
+// console.log(3-3)
+// console.log("3" - "3")
+// console.log(3 - "3")
+// console.log("3" - 3)
+// console.log(3 - - "3")
+// console.log("3" - - 3)
+// console.log(3 + - 3)
+// console.log(3 - + 3)
+
+
+// variable declaration methods:
+// let, const & var
+// "use strict";
+
+// let x = 10
+// console.log(x)
+// x = 20
+// console.log(x)
+
+
+// var x = 10
+// console.log(x)
+// x = 20
+// console.log(x)
+
+
+// const x = 10
+// console.log(x)
+// x = 20
+// console.log(x)
